adfed23217126d0cef6384c05400e079d3cf2a22
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2022-05-31 16:27:34 +0300

    Base package

---
+++ payselection.php
@@ -0,0 +1,185 @@
+<?php
+/*
+ * @package     RadicalMart Package
+ * @subpackage  plg_radicalmart_payment_payselection
+ * @version     __DEPLOY_VERSION__
+ * @author      Delo Design - delo-design.ru
+ * @copyright   Copyright (c) 2022 Delo Design. All rights reserved.
+ * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ * @link        https://delo-design.ru/
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Http\Http;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Log\Log;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Uri\Uri;
+use Joomla\Registry\Registry;
+
+class plgRadicalMart_PaymentPayselection extends CMSPlugin
+{
+	/**
+	 * Loads the application object.
+	 *
+	 * @var  CMSApplication
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected $app = null;
+
+	/**
+	 * Loads the database object.
+	 *
+	 * @var  JDatabaseDriver
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $db = null;
+
+	/**
+	 * Affects constructor behavior.
+	 *
+	 * @var  boolean
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * Payment method params.
+	 *
+	 * @var  Registry
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected $_paymentMethodParams = null;
+
+	/**
+	 * Prepare order shipping method data.
+	 *
+	 * @param   string  $context   Context selector string.
+	 * @param   object  $method    Method data.
+	 * @param   array   $formData  Order form data.
+	 * @param   array   $products  Order products data.
+	 * @param   array   $currency  Order currency data.
+	 *
+	 * @throws  Exception
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public function onRadicalMartGetPaymentMethods($context, $method, $formData, $products, $currency)
+	{
+		// Set disabled
+		$method->disabled = false;
+
+		// TODO: Clean secret param
+		$method->params->set('public_id', '');
+		$method->params->set('api_secret', '');
+		$method->params->set('inn', '');
+
+		// Set order
+		$method->order              = new stdClass();
+		$method->order->id          = $method->id;
+		$method->order->title       = $method->title;
+		$method->order->code        = $method->code;
+		$method->order->description = $method->description;
+		$method->order->price       = array();
+	}
+
+	/**
+	 * Check can order pay.
+	 *
+	 * @param   string  $context  Context selector string.
+	 * @param   object  $order    Order Item data.
+	 *
+	 * @throws  Exception
+	 *
+	 * @return boolean True if can pay, False if not.
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public function onRadicalMartCheckOrderPay($context, $order)
+	{
+		// Check order payment method
+		if (empty($order->payment)
+			|| empty($order->payment->id)
+			|| empty($order->payment->plugin)
+			|| $order->payment->plugin !== 'payselection') return false;
+
+		// Check method params
+		$params = $this->getPaymentMethodParams($order->payment->id);
+		// TODO Check params if (empty($params->get('public_id')) || empty($params->get('api_secret'))) return false;
+
+		// Check order status
+		if (empty($order->status->id) || !in_array($order->status->id, $params->get('payment_available', array())))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Method to get payment method params.
+	 *
+	 * @param   int  $pk  Payment method id.
+	 *
+	 * @return Registry Payment method params
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected function getPaymentMethodParams($pk = null)
+	{
+		$pk = (int) $pk;
+		if (empty($pk)) return new Registry();
+
+		if ($this->_paymentMethodParams === null) $this->_paymentMethodParams = array();
+		if (!isset($this->_paymentMethodParams[$pk]))
+		{
+			$db                              = $this->db;
+			$query                           = $db->getQuery(true)
+				->select('params')
+				->from($db->quoteName('#__radicalmart_payment_methods'))
+				->where('id = ' . $pk);
+			$this->_paymentMethodParams[$pk] = ($result = $db->setQuery($query, 0, 1)->loadResult())
+				? new Registry($result) : new Registry();
+		}
+
+		return $this->_paymentMethodParams[$pk];
+	}
+
+	/**
+	 * Method to create order in RadicalMart.
+	 *
+	 * @param   object    $order   Order data object.
+	 * @param   Registry  $params  RadicalMart component params.
+	 * @param   array     $links   RadicalMart plugin links.
+	 *
+	 * @throws  Exception
+	 *
+	 * @return  array  Payment redirect data on success.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function onRadicalMartPay($order, $links, $params)
+	{
+		$result = array(
+			'pay_instant' => true,
+			'link'        => false,
+		);
+
+		// Check order payment method
+		if (empty($order->payment)
+			|| empty($order->payment->id)
+			|| empty($order->payment->plugin)
+			|| $order->payment->plugin !== 'payselection') return $result;
+
+		echo '<pre>', print_r($order, true), '</pre>';
+		exit('stop');
+	}
+}
---
+++ script.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * @package     RadicalMart Package
+ * @subpackage  plg_radicalmart_payment_payselection
+ * @version     __DEPLOY_VERSION__
+ * @author      Delo Design - delo-design.ru
+ * @copyright   Copyright (c) 2022 Delo Design. All rights reserved.
+ * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ * @link        https://delo-design.ru/
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Installer\InstallerAdapter;
+
+class PlgRadicalMart_PaymentPayselectionInstallerScript
+{
+	/**
+	 * Runs right after any installation action.
+	 *
+	 * @param   string            $type    Type of PostFlight action. Possible values are:
+	 * @param   InstallerAdapter  $parent  Parent object calling object.
+	 *
+	 * @throws  Exception
+	 *
+	 * @return  boolean True on success, False on failure.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	function postflight($type, $parent)
+	{
+		// Enable plugin
+		if ($type == 'install') $this->enablePlugin($parent);
+
+		return true;
+	}
+
+	/**
+	 * Enable plugin after installation.
+	 *
+	 * @param   InstallerAdapter  $parent  Parent object calling object.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected function enablePlugin($parent)
+	{
+		// Prepare plugin object
+		$plugin          = new stdClass();
+		$plugin->type    = 'plugin';
+		$plugin->element = $parent->getElement();
+		$plugin->folder  = (string) $parent->getParent()->manifest->attributes()['group'];
+		$plugin->enabled = 1;
+
+		// Update record
+		Factory::getDbo()->updateObject('#__extensions', $plugin, array('type', 'element', 'folder'));
+	}
+}
