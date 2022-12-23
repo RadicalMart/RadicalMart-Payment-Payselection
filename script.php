ada75c3f84e4ea36f5b051b50994b748e864305b
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2022-12-23 16:15:37 +0300

    Change update server

--- script.php
+++ script.php
@@ -16,6 +16,30 @@ use Joomla\CMS\Installer\InstallerAdapter;
 
 class PlgRadicalMart_PaymentPayselectionInstallerScript
 {
+
+	/**
+	 * Runs right before any installation action.
+	 *
+	 * @param   string            $type    Type of PostFlight action.
+	 * @param   InstallerAdapter  $parent  Parent object calling object.
+	 *
+	 * @throws  Exception
+	 *
+	 * @return  boolean True on success, False on failure.
+	 *
+	 * @since  1.0.1
+	 */
+	function preflight($type, $parent)
+	{
+		// Change update servers
+		if ($type === 'update')
+		{
+			$this->changeUpdateServer();
+		}
+
+		return true;
+	}
+
 	/**
 	 * Runs right after any installation action.
 	 *
@@ -54,5 +78,28 @@ class PlgRadicalMart_PaymentPayselectionInstallerScript
 
 		// Update record
 		Factory::getDbo()->updateObject('#__extensions', $plugin, array('type', 'element', 'folder'));
+	}
+
+	/**
+	 * Method to change update server.
+	 *
+	 * @since 1.0.1
+	 */
+	protected function changeUpdateServer()
+	{
+		$old = 'https://radicalmart.ru/update?element=plg_radicalmart_payment_payselection';
+		$new = 'https://sovmart.ru/update?element=plg_radicalmart_payment_payselection';
+
+		$db    = Factory::getDbo();
+		$query = $db->getQuery(true)
+			->select(['update_site_id', 'location'])
+			->from($db->quoteName('#__update_sites'))
+			->where($db->quoteName('location') . ' LIKE ' .
+				$db->quote($old));
+		if ($update = $db->setQuery($query)->loadObject())
+		{
+			$update->location = $new;
+			$db->updateObject('#__update_sites', $update, 'update_site_id');
+		}
 	}
 }
